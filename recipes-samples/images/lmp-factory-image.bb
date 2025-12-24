DynamicDevices/meta-dynamicdevices-distro

commit 6697f52b22f3c0e76937b0ee00ae55bb21633810

Fix fio user home directory creation - use ROOTFS_POSTPROCESS_COMMAND
EXTRA_USERS_PARAMS does not accept shell commands like mkdir.
Use ROOTFS_POSTPROCESS_COMMAND to create /var/rootdirs/home before
useradd runs. This ensures the parent directory exists when useradd -m
creates the home directory.

recipes-samples/images/lmp-factory-image.bb
@@ -197,8 +197,14 @@ IMAGE_FEATURES += "ssh-server-openssh"
 # Disable root login - NOTE: This means we can't use "sudo su" any more *but* running commands as root works
 inherit extrausers
 
+# Create parent directory for fio user home before useradd runs
+create_fio_home_dir() {
+    mkdir -p ${IMAGE_ROOTFS}/var/rootdirs/home
+}
+
+ROOTFS_POSTPROCESS_COMMAND:prepend = "create_fio_home_dir; "
+
 EXTRA_USERS_PARAMS:append = "\
-  mkdir -p /var/rootdirs/home; \
   useradd -r -m -d /var/rootdirs/home/fio -s /bin/sh -G sudo,audio,plugdev,users,docker,dialout fio; \
   usermod -s /sbin/nologin root; \
 "

commit 89c53040569e714d73c7ea70fd5eeb8d6f0d8544

Fix fio home directory ownership - set parent directory ownership correctly
- Set /var/rootdirs/home ownership to fio user (uid 1000) when creating it
- This ensures useradd -m creates /var/rootdirs/home/fio with correct ownership
- Fixes issue introduced in commit 6697f52 where parent was created as root
- Simpler solution than post-processing ownership fix

recipes-samples/images/lmp-factory-image.bb
@@ -198,23 +198,16 @@ IMAGE_FEATURES += "ssh-server-openssh"
 inherit extrausers
 
 # Create parent directory for fio user home before useradd runs
-# Note: useradd -m will create the home directory, but we need to ensure
-# the parent directory exists. After useradd, we fix ownership in case
-# the directory was pre-created.
+# Create it with correct ownership (fio user, uid 1000) so useradd -m
+# creates the home directory with proper ownership
 create_fio_home_dir() {
     mkdir -p ${IMAGE_ROOTFS}/var/rootdirs/home
-}
-
-fix_fio_home_ownership() {
-    # Ensure fio home directory is owned by fio user (uid 1000)
-    # This fixes the case where the directory was pre-created as root
-    if [ -d "${IMAGE_ROOTFS}/var/rootdirs/home/fio" ]; then
-        chown -R 1000:1000 ${IMAGE_ROOTFS}/var/rootdirs/home/fio
-    fi
+    # Set ownership to fio user (uid 1000) so when useradd -m creates
+    # /var/rootdirs/home/fio, it inherits correct parent ownership
+    chown 1000:1000 ${IMAGE_ROOTFS}/var/rootdirs/home 2>/dev/null || true
 }
 
 ROOTFS_POSTPROCESS_COMMAND:prepend = "create_fio_home_dir; "
-ROOTFS_POSTPROCESS_COMMAND:append = "fix_fio_home_ownership; "
 
 EXTRA_USERS_PARAMS:append = "\
   useradd -r -m -d /var/rootdirs/home/fio -s /bin/sh -G sudo,audio,plugdev,users,docker,dialout fio; \